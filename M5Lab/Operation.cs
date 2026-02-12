dbbc2db3bb2e96417d85ad1a8b0efcc65196e56b
Daniel <dxb718@psu.edu>
2026-02-12 09:56:51 -0500

    m6

--- M5Lab/Operation.cs
+++ M5Lab/Operation.cs
@@ -2,8 +2,10 @@
 {   
     protected static double value = 0;
 
+    // no parameter
     public virtual void compute() {}
-
+    
+    // 1 parameter
     public abstract void compute(double x);
 
     public double getValue()
---
+++ M6Lab/M6Lab/Edge.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M6Lab
+{
+    internal class Edge
+    {
+        private int edge_ID { get; set; }
+        private Vertex from_vertex { get; set; }
+        private Vertex to_vertex { get; set; }
+
+        public Edge() { 
+            from_vertex = new Vertex();
+            to_vertex = new Vertex();
+        }
+
+        public void drawing()
+        {
+            // TODO
+        }
+    }
+}

b7e686aa0fc1aafcde51879185ae20ac477ce12b
Miguel Quemado <mquemado16@gmail.com>
2026-02-12 10:09:41 -0500

    fixes

--- M5Lab/OperationFactoryIF.cs
+++ M5Lab/OperationFactoryIF.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,13 +5,7 @@ using System.Text;
 namespace M5Lab
 {
     public interface OperationFactoryIF
-    {
-        public OperationIF create(string className);
-    }
-}
-=======
-interface OperationFactoryIF
 {
     public OperationIF create(string className);
 }
->>>>>>> b63706590b4a58bed05c154f6834a93516c9fe37
+}

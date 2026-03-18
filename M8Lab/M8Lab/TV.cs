b1ebf4f01e0b8e5d60f53b8b7ed74f9b43fce84e
Daniel <dxb718@psu.edu>
2026-03-18 14:09:08 -0400

    m8 init

---
+++ M8Lab/M8Lab/TV.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M8Lab
+{
+    internal class TV
+    {
+        private int MSRP;
+        private string Type;
+
+        public TV(int MSRP, string Type) {
+            this.MSRP = MSRP;
+            this.Type = Type;
+        }
+
+        public TV replenish(string type, int budget)
+        {   
+            // placeholder
+            return new SmartTV();
+        }
+
+        public string getType()
+        {
+            return Type;
+        }
+
+        public int getPrice()
+        {
+            return MSRP;
+        }
+
+        public string getInfo()
+        {
+            return "Type: " + getType() + " | Price: " + getPrice();
+        }
+
+        protected class SmartTV : TV
+        {
+        }
+        protected class UltraTV : TV
+        {
+        }
+    }
+}

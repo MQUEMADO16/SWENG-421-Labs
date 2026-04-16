3f41b948e3b07d275f982484d6d1be818aae202e
Daniel <dxb718@psu.edu>
2026-04-16 08:35:49 -0400

    m11 work

---
+++ M11Lab/M11Lab/CalculatorState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M11Lab
+{
+    internal abstract class CalculatorState
+    {
+        public static Calculator calculator;
+        public double num1;
+        public double num2;
+        public Operator operation;
+
+        public void nextOperation(Operator operation)
+        {
+            this.operation = operation;
+        }
+    }
+}
---
+++ M11Lab/M11Lab/Divide.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M11Lab
+{
+    internal class Divide : Operator
+    {
+        public double calc(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}

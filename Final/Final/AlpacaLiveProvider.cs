82cd3bf4d854fd2aef7cb7a84ea5164a441074eb
badach02 <dxb718@psu.edu>
2026-04-29 16:45:35 -0400

    api work

--- Final/Final/AlpacaLiveProvider.cs
+++ Final/Final/AlpacaLiveProvider.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DotNetEnv;
+using Alpaca.Markets;
 
 namespace Final
 {
     internal class AlpacaLiveProvider
     {
+        private readonly IAlpacaDataClient? client;
+        private List<string> tickers = new List<string>();
+
+        public AlpacaLiveProvider()
+        {
+            Env.Load();
+            var apiKey = Environment.GetEnvironmentVariable("ALPACA_KEY");
+            var secret = Environment.GetEnvironmentVariable("ALPACA_SECRET");
+
+            if (apiKey != null && secret != null)
+            {
+                var secretKey = new SecretKey(apiKey, secret);
+                client = Environments.Paper.GetAlpacaDataClient(secretKey);
+            }
+        }
+
+        public void connect()
+        {
+
+
+
+        }
+
+        public void subscribe(string ticker)
+        {
+            tickers.Add(ticker);
+        }
+
+        public void disconnect()
+        {
+
+        }
     }
 }
---
+++ Final/Final/TradeDataPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NodaTime;
+
+namespace Final
+{
+    internal class TradeDataPoint
+    {
+        private string ticker { get; set; }
+        private double currentPrice { get; set; }
+        private long volume { get; set; }
+        private Instant timestamp { get; set; }
+
+        public TradeDataPoint()
+        {
+
+        }
+    }
+}

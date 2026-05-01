6e65494bde83201fe2c33ff2561293adac4e5ae4
badach02 <dxb718@psu.edu>
2026-05-01 19:38:10 -0400

    classes

---
+++ Final/Final/PriceEvaluationFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final
+{
+    internal class PriceEvaluationFilter
+    {
+        public bool getData(double low, double high, TradeDataPoint data)
+        {
+            if (low <= data.Price  && data.Price <= high)
+            {
+                return true;
+            }
+            else
+            {
+                return false; 
+            }
+        }
+    }
+}
